005e4a8dd9b824f6a9ed6ecfbdcf7e7d02ae42c7
Nikodemus <nikodembilinski4@gmail.com>
2025-12-10 00:32:07 +0100

    Funkcja zapisu i wyboru notatki do poprawienia na pewno bo przy dodaniu usuniecia notatki wszystko sie skupsztali

--- Aplikacja Notatek/Okno_Notatki.xaml.cs
+++ Aplikacja Notatek/Okno_Notatki.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,21 +22,46 @@ namespace Aplikacja_Notatek
     public partial class Okno_Notatki : Window
     {
         private int counternotatki = 0;
+        private int numberofnotes = 0;
+
         public Okno_Notatki()
         {
-            InitializeComponent();
 
-            
+            InitializeComponent();
+            while (File.Exists("Notatka" + counternotatki + ".txt") == true)
+            {
+                ListaNotatek.Items.Add("Notatka" + counternotatki + ".txt \n");
+                counternotatki++;
+            }
+            counternotatki = ListaNotatek.Items.Count;
         }
         private void Zapisz_Notatke(object sender, RoutedEventArgs e)
         {
             
             string Tekst_Notatki = PoleNotatki.Text;
+            string temp;
+            if(ListaNotatek.Items.Count != 0)
+            {
+                temp = ListaNotatek.SelectedItem.ToString();
+                temp = Regex.Match(temp, @"\d+").Value;
+
+                counternotatki = int.Parse(temp);
+            }
+            
 
             File.WriteAllText("Notatka" + counternotatki + ".txt", Tekst_Notatki);
-            counternotatki++;
+            numberofnotes++;
 
             MessageBox.Show("Notatka została zapisana!");
+
+            counternotatki = ListaNotatek.Items.Count;
+            while (File.Exists("Notatka" + counternotatki + ".txt") == true)
+            {
+                ListaNotatek.Items.Add("Notatka" + counternotatki + ".txt \n");
+                counternotatki++;
+            }
+            counternotatki = ListaNotatek.Items.Count;
+
         }
 
         private void Powrot_Menu_Glowne(object sender, RoutedEventArgs e)
@@ -45,14 +71,12 @@ namespace Aplikacja_Notatek
 
         private void Wybor_Notatki(object sender, RoutedEventArgs e)
         {
-            while (File.Exists("Notatka" + counternotatki + ".txt"))
+            string wybor = ListaNotatek.SelectedItem.ToString();
+            if(wybor != null)
             {
-                ListaNotatek.Text += "Notatka" + counternotatki + ".txt \n";
-                counternotatki++;
+                PoleNotatki.Text = File.ReadAllText(wybor.Trim());
             }
 
-            
-           
         }
     }
 }
